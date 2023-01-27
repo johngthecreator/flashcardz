d4b3b09b20cdd95ed2ef79a9335d3be7798f566f
John Gorriceta <79299314+jongee0w0@users.noreply.github.com>
2023-01-26 17:03:25 -0700

    ghetto switch button

--- src/routes/Practice.svelte
+++ src/routes/Practice.svelte
@@ -6,34 +6,40 @@
   if (localStorage.getItem('cards')){
     cardCache = JSON.parse(localStorage.getItem('cards'));
   }
+  let switchCards = true;
 </script>
 
 <main>
   <CardInput />
-  <!-- {#if ($cardList.length >= 1)}
-    {#each $cardList as card}
-      <Flashcard title={card.item.split(":")[0] ? card.item.split(":")[0].replace("-",""):"" } def={card.item.split(":")[1] ? card.item.split(":")[1]:""} />
-    {/each}
-  {:else if (cardCache)}
-    {#each cardCache as card}
-      <Flashcard title={card.item.split(":")[0] ? card.item.split(":")[0].replace("-",""):"" } def={card.item.split(":")[1] ? card.item.split(":")[1]:""} />
-    {/each}
+  <button on:click={()=>{(switchCards ? switchCards = false: switchCards = true)}}>Switch Cards</button>
+  {#if switchCards}
+    {#if ($cardList.length >= 1)}
+      {#each $cardList as card}
+        <Flashcard title={card.item.split(":")[0] ? card.item.split(":")[0].replace("-",""):"" } def={card.item.split(":")[1] ? card.item.split(":")[1]:""} />
+      {/each}
+    {:else if (cardCache)}
+      {#each cardCache as card}
+        <Flashcard title={card.item.split(":")[0] ? card.item.split(":")[0].replace("-",""):"" } def={card.item.split(":")[1] ? card.item.split(":")[1]:""} />
+      {/each}
+    {:else}
+      <h3>Make A New Flashcard Set!</h3>
+    
+    {/if}
+
   {:else}
-    <h3>Make A New Flashcard Set!</h3>
-  
-  {/if} -->
 
   <!-- This is a ghetto feature of def on front and terms on back -->
-  {#if ($cardList.length >= 1)}
-    {#each $cardList as card}
-      <Flashcard title={card.item.split(":")[1] ? card.item.split(":")[1].replace("-",""):"" } def={card.item.split(":")[0] ? card.item.split(":")[0]:""} />
-    {/each}
-  {:else if (cardCache)}
-    {#each cardCache as card}
-      <Flashcard title={card.item.split(":")[1] ? card.item.split(":")[1].replace("-",""):"" } def={card.item.split(":")[0] ? card.item.split(":")[0]:""} />
-    {/each}
-  {:else}
-    <h3>Make A New Flashcard Set!</h3>
+    {#if ($cardList.length >= 1)}
+      {#each $cardList as card}
+        <Flashcard title={card.item.split(":")[1] ? card.item.split(":")[1].replace("-",""):"" } def={card.item.split(":")[0] ? card.item.split(":")[0]:""} />
+      {/each}
+    {:else if (cardCache)}
+      {#each cardCache as card}
+        <Flashcard title={card.item.split(":")[1] ? card.item.split(":")[1].replace("-",""):"" } def={card.item.split(":")[0] ? card.item.split(":")[0]:""} />
+      {/each}
+    {:else}
+      <h3>Make A New Flashcard Set!</h3>
+    {/if}
   {/if}
   <!---End of ghetto feature--->
 
